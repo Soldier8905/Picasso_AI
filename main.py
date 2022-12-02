15b5a4114cdd955bf71ded40ff9684bfc4512c66
Soldier8905 <100417305+Soldier8905@users.noreply.github.com>
2022-12-02 14:21:55 +0400

    added functions that have to be worked on

--- main.py
+++ main.py
@@ -1,39 +1,54 @@
 import random
 from PIL import Image, ImageDraw
 
-def create_new_points(points:list, point_connections:list, fraction:float) -> list:
+
+def create_new_points(points: list, point_connections: list, fraction: float) -> list:
     n_points = []
     for i in point_connections:
-        n_points.append([points[i[0]][0]+(points[i[1]][0]-points[i[0]][0])*fraction,points[i[0]][1]+(points[i[1]][1]-points[i[0]][1])*fraction])
+        n_points.append(
+            [
+                points[i[0]][0] + (points[i[1]][0] - points[i[0]][0]) * fraction,
+                points[i[0]][1] + (points[i[1]][1] - points[i[0]][1]) * fraction,
+            ]
+        )
     return n_points
 
-def draw_connections(point:list,point_connections:list,draw:ImageDraw.ImageDraw,width:int):
-    try:
-        for i in point_connections:
-            draw.line((tuple(point[i[0]]),tuple(point[i[1]])),width=width,fill=1)
-    except:
-        pass
-    
-def generate(points,point_connections,fraction,draw,recersion):
+
+def create_connections(points):
+    # generate the connections between a inputed list of points
+    pass
+
+
+def find_shapes(points):
+    # make it so that it can take all points as input and output a list of shapes made up of inputed points
+    pass
+
+
+def draw_connections(
+    point: list, point_connections: list, draw: ImageDraw.ImageDraw, width: int
+):
+    for i in point_connections:
+        draw.line((tuple(point[i[0]]), tuple(point[i[1]])), width=width, fill=1)
+
+
+def generate(points, point_connections, fraction, draw, recersion):
     if recersion > 0:
-        points = create_new_points(points,point_connections,fraction)
-        draw_connections(points,point_connections,draw,1)
-        generate(points,point_connections,fraction,draw,recersion-1)
+        points = create_new_points(points, point_connections, fraction)
+        draw_connections(points, point_connections, draw, 1)
+        generate(points, point_connections, fraction, draw, recersion - 1)
 
 
-
-
-def main(size:tuple,fraction:float,recersion_limit:int):
-    img = Image.new('1', size, 0)
+def main(size: tuple, fraction: float, recersion_limit: int):
+    img = Image.new("1", size, 0)
     draw = ImageDraw.Draw(img)
 
-    points = [[0,0],[size[0],0],[size[0],size[1]],[0,size[1]]]
-    point_connections = [[0,1],[1,2],[2,3],[3,0]]
+    points = [[0, 0], [size[0], 0], [size[0], size[1]], [0, size[1]]]
+    point_connections = [[0, 1], [1, 2], [2, 3], [3, 0]]
+    draw_connections(points, point_connections, draw, 10)
 
-    draw_connections(points,point_connections,draw,10)
-
-    generate(points,point_connections,fraction,draw,recersion_limit)
+    generate(points, point_connections, fraction, draw, recersion_limit)
 
     img.show()
 
-main((1000,1000),0.25,10)
+
+main((1000, 1000), 0.25, 10)
